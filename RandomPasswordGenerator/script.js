61b1e3305eeb01bea8542ff88567f6efd0c24a41
Nisip Sigdel <103618920+nishipsigdel@users.noreply.github.com>
2024-12-08 22:55:01 +0545

    Add files via upload

---
+++ RandomPasswordGenerator/script.js
@@ -0,0 +1,38 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "~`!@#$%^&*()_-+={[}]|\:;'<,>.?/";
+
+const allChars = upperCase + lowerCase + numbers + symbols;
+
+function generatePassword(){
+    let password = "";
+    // password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    // password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    // password += numbers[Math.floor(Math.random() * numbers.length)];
+    // password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    passwordBox.value = password;
+} 
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");    
+
+////
+
+    const tooltipText = document.getElementById("tooltipText");
+    tooltipText.textContent = "Copied!";
+    
+    // Reset the tooltip text after 2 seconds
+    setTimeout(() => {
+        tooltipText.textContent = "Copy";
+    }, 2000);
+}
